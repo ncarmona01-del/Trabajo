56e50553259e39a1465bdb57a566143808fc12c4
Taller <n.carmona01@ufromail.cl>
2025-09-04 12:52:22 -0400

    primera parte

--- src/main/java/VentanaSaludo.java
+++ src/main/java/VentanaSaludo.java
@@ -6,33 +6,34 @@ public class VentanaSaludo {
     public static void main(String[] args) {
 
         JFrame ventana = new JFrame("app de saludo icc490");
-        ventana.setSize(400, 200);
+        ventana.setSize(600, 500);
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ventana.setLayout(null);
 
-
         JTextField campotexto = new JTextField();
-        campotexto.setBounds(50,30,200,25);
+        campotexto.setBounds(100,60,200,25);
 
         JButton botonSaludar = new JButton("Saludar");
-        botonSaludar.setBounds(270, 30, 100, 25);
+        botonSaludar.setBounds(300, 60, 100, 25);
 
         JLabel etiquetaSaludo = new JLabel("");
-        etiquetaSaludo.setBounds(50, 80, 300, 25 );
+        etiquetaSaludo.setBounds(100, 200, 300, 25 );
 
-        botonSaludar.addActionListener(e -> {
+        botonSaludar.addActionListener(_ -> {
             String nombre = campotexto.getText();
             etiquetaSaludo.setText("Hola: " + nombre );
-        });
+                if (nombre.trim(). isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Debe ingresar un nombre");
+                } else {
+                    etiquetaSaludo.setText("Hola: " + nombre);
+                }
+        }
 
         ventana.add(campotexto);
         ventana.add(botonSaludar);
         ventana.add(etiquetaSaludo);
         ventana.setLocationRelativeTo(null);
         ventana.setVisible(true);
-
-
-
     }
 }
 
